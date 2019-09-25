e1ec872f3e118c2a0593354a453e21e2526cb3f8
Roxanne Chambers <roxanne.chambers@kearsarge.org>
2019-09-25 11:26:02 -0400

    Deleted the sentences of story 1

--- Story2.py
+++ Story2.py
@@ -4,13 +4,10 @@ def Story1(debug = False):
     if debug: print("Story1 Function") 
 
     print("\n")
-    friendName1 = getWord("Enter a name ", debug)
-    sport1 = getSport("Enter a sport ", debug)
-    
+   
     out = "\n"
     out += "\n"
-    out+= "One day me and my friend " + friendName1
-    out+= " Were out playing " + sport1
+    
     
     
     
