3cdf3be51836733926636611e1bbb5d6f9ecee34
Roxanne Chambers <roxanne.chambers@kearsarge.org>
2019-09-25 11:20:32 -0400

    A

---
+++ Madlibs.py
@@ -0,0 +1,41 @@
+from Screens import *
+from Getters import *
+from Story1 import *
+
+
+
+
+def Madlibs (debug = False):
+    if debug: print ("welcome to debug")
+    
+    print(TitleScreen(debug))
+    input ("press enter to continue")
+
+    done = False
+    
+    while not done:
+        print(MainMenu(debug))
+        choice = getMenuOption(debug)
+       
+        if choice == "q":
+            exit();
+            
+        elif choice == "1":
+            print(Story1())
+            print("\n") 
+            input("press enter to continue")
+            
+        elif choice == "unicorn":
+            print("You have found my magical land")
+            input("press enter to continue")
+            
+            
+            
+
+
+
+
+
+
+Madlibs (True)
+
