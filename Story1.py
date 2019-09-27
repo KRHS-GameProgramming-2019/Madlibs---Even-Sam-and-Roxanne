bb83a4a0ed1b4a980d351b2ce3b0f06f3b4f30a5
Roxanne Chambers <roxanne.chambers@kearsarge.org>
2019-09-27 11:27:18 -0400

    added more!

--- Story1.py
+++ Story1.py
@@ -7,19 +7,25 @@ def Story1(debug = False):
     friendName1 = getWord("Enter a name ", debug)
     sport1 = getSport("Enter a sport ", debug)
     Place1= getPlace("enter a place (proper noun) ")
-    animal1 = getAnimal("enter an animal")
+    animal1 = getAnimal("enter an animal ")
     
     out = "\n"
     out += "\n"
     out+= "One day me and my friend " + friendName1
     out+= " Were out playing " + sport1 
     out+= ". "
+    out+= "\n"
     out += "Me and my homie cuh, " + friendName1
     out+= " went to " + Place1 
     out += " to play" + sport1
     out += ". "
+    out += "\n"
     out += "while we were playing" + sport1
-    out += " we saw an animal" +animal1
+    out += " we saw a" +animal1
+    out += " walking near" + Place1
+    out += ". "
+    out += "\n"
+    
     
     
     
