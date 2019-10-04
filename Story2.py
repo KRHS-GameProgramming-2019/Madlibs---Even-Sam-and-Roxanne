da3728d15565bb5be63845fc3b2d178d941af8a0
ThePinkLloyd <samuel.lloyd@kearsarge.org>
2019-10-04 10:55:14 -0400

    Fixed an error in Story 2 that was preventing it from working. Remember that when you do an indent, it's ("\n") . Yeet

--- Story2.py
+++ Story2.py
@@ -17,11 +17,11 @@ def Story2(debug = False):
     out = "\n"
     out += "\n"
     
-    out+= "Once upon a time, you didn't go to Kearsarge, and you never had" + Teacher1 + " as a teacher." + "\n" 
-    out+= "In this world every grade you get was a" + grade1 + "and you lived a happy life." + "\n"
-    out+= "You live in " + DreamHome + " and you can eat as much " + dreamFood + " as you want." + "\n"
-    out+= "But, then you woke up, and your average grade was " + grade2 + " and you live in" + realtown ".\n"
-    out+= "Yet, at the end of the day you are still eating" + dreamFood ", YAY. \n"
+    out+= "Once upon a time, you didn't go to Kearsarge, and you never had" + Teacher1 + " as a teacher." + ("\n")
+    out+= "In this world every grade you get was a" + grade1 + "and you lived a happy life." + ("\n")
+    out+= "You live in " + DreamHome + " and you can eat as much " + dreamFood + " as you want." + ("\n")
+    out+= "But, then you woke up, and your average grade was " + grade2 + " and you live in" + realtown + ("\n")
+    out+= "Yet, at the end of the day you are still eating" + dreamFood + ", YAY." + ("\n")
     
     
     
